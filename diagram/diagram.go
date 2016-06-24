54270156e31b5ee748e1e81894de07fa3fa8a9d5
Dany Alejandro Cabrera <otello2040@gmail.com>
2016-06-24 14:42:34 -0700

    added PrintLogs bool to activate / deactivate logging

--- diagram/diagram.go
+++ diagram/diagram.go
@@ -2,33 +2,59 @@ package diagram
 
 import "fmt"
 
+var PrintLogs bool = true // change to "false" to stop printing log messages. Use this to activate and deactivate log messages.
+
 // Produces an arrow from origin to destination with the specified label
 func LogMsg(origin string, dest string, label string) {
-	fmt.Println("@|msg|" + origin + "|->|" + dest + "|" + label)
+	if (PrintLogs) {
+		fmt.Println("@|msg|" + LogName(origin) + "|->|" + LogName(dest) + "|" + label)
+	}
 }
 
 // Produces a title with the specfied label
 func LogTitle(label string) {
-	fmt.Println("@|title|" + label)
+	if (PrintLogs) {
+		fmt.Println("@|title|" + label)
+	}
 }
 
 // Produces a state circle with the specfied color and label.
 // color can be: "red", "green", "yellow"
 func LogState(color string, node string, label string) {
-	fmt.Println("@|state|" + color + "|" + node + "|" + label)
+	if (PrintLogs) {
+		fmt.Println("@|state|" + color + "|" + LogName(node) + "|" + label)
+	}
 }
 
 // Produces an information icon with a text tooltip
 func LogInfo(node string, label string) {
-	fmt.Println("@|info|" + node + "|" + label)
+	if (PrintLogs) {
+		fmt.Println("@|info|" + LogName(node) + "|" + label)
+	}
 }
 
 // Produces a table around a text of formated key:value pairs. Example: "A:23, B:45, C:63"
 func LogTable(node string, tableText string) {
-	fmt.Println("@|table|" + node + "|" + tableText)
+	if (PrintLogs) {
+		fmt.Println("@|table|" + LogName(node) + "|" + tableText)
+	}
 }
 
 // Skips some vertical space; useful for separating events in the diagram
 func LogNext() {
-	fmt.Println("@|next")
+	if (PrintLogs) {
+		fmt.Println("@|next")
+	}
+}
+
+// Translates the full name of a node to an equivalent ID for the diagram.
+// Modify this as needed for your project
+func LogName(name string) string {
+	switch (name) {
+	case "vs":
+		return "vs"
+	case "ck":
+		return "ck"
+	}
+	return "s" + fmt.Sprintf("%c", name[len(name) - 1])
 }
